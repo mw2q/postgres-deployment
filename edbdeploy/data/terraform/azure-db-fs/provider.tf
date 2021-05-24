fd993fcaf8f6aca5096592c94ebc237f9c513bad
Mark Wong <mark.wong@enterprisedb.com>
2021-05-24 09:40:00 -0700

    Update deprecated version requirements

--- edbdeploy/data/terraform/azure-db-fs/provider.tf
+++ edbdeploy/data/terraform/azure-db-fs/provider.tf
@@ -1,4 +1,3 @@
 provider "azurerm" {
-  version = "=2.57.0"
   features {}
 }
--- edbdeploy/data/terraform/azure-db-fs/versions.tf
+++ edbdeploy/data/terraform/azure-db-fs/versions.tf
@@ -2,6 +2,7 @@ terraform {
   required_providers {
     azurerm = {
       source = "hashicorp/azurerm"
+      version = ">=2.57.0"
     }
   }
   required_version = ">= 0.13"
